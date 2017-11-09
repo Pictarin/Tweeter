63b15ca20f11ff330d5ca726e0f2c303a9a071f9
Pictarin <kevin.adam@clear-it.de>
2017-11-09 08:03:57 +0100

    Updated DB

--- src/tweeter/Application.java
+++ src/tweeter/Application.java
@@ -1,13 +1,9 @@
 package tweeter;
 
-import database.Database;
-
 public class Application {
 	private static MainFrame frame;
-	private static Database db;
 	
 	public static void main(String[] args) {
-		//db = new Database();
 		frame = new MainFrame();
 	}
 }
